2cadca8e84fcbc535773fbe23bd2908a29a5e877
rhykeiu <56758299+rhykeiu@users.noreply.github.com>
2020-05-19 23:03:53 +0100

    Update link.js
    Changed link address.

--- Web Application/crud-express/public/js/link.js
+++ Web Application/crud-express/public/js/link.js
@@ -1,10 +1,10 @@
 
 function linkNewProject(){
-	window.location = 'http://127.0.0.1:3000/views/index.html';
+	window.location = 'http://127.0.0.1:3000/students/index';
 }
 function linkUser(){
 	window.location = 'http://127.0.0.1:3000/views/user.html';
 }
 function linkProjectList(){
-	window.location = 'http://127.0.0.1:3000';
+	window.location = 'http://127.0.0.1:3000/students';
 }
