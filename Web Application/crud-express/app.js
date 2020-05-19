171892d47fc0ee2e2719924c60feb01ae902ca53
rhykeiu <56758299+rhykeiu@users.noreply.github.com>
2020-05-19 23:21:52 +0100

    Update app.js

--- Web Application/crud-express/app.js
+++ Web Application/crud-express/app.js
@@ -1,28 +1,27 @@
 var express = require('express')
-var fs = require('fs')
+var router = require('./router')
+var bodyParser = require('body-parser')
 
 var app = express()
 
 app.use('/node_modules/', express.static('./node_modules/'))
 app.use('/public/', express.static('./public/'))
-app.use('/views/', express.static('./views/'))
 
 app.engine('html', require('express-art-template'))
 
-app.get('/', function (req, res) {
-    fs.readFile('./db.json', 'utf8', function (err, data) {
-        if(err){
-            return res.status(500).send('server error.')
-        }
 
-        var projects = JSON.parse(data).projects
-        res.render('project_list.html', {
-            projects: projects
-        })
-    })
+app.use(bodyParser.urlencoded({ extended: false }))
 
-})
+app.use(bodyParser.json())
+
+app.use(router)
+
 
 app.listen(3000, function () {
-    console.log('running...')
+    console.log('running 3000...')
 })
+
+module.exports = app
+
+
+
